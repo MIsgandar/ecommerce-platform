dcc247018b690a3040a6b6dbecc8451b4178e63b
MIsgandar <miri.isgadnarli@gmail.com>
2026-08-17 12:59:39 +0200

    same s**t different pile

---
+++ services/product-service/src/main/java/com/ecommerce/productservice/controller/ProductController.java
@@ -0,0 +1,67 @@
+package com.ecommerce.productservice.controller;
+
+import com.ecommerce.productservice.dto.CreateProductRequest;
+import com.ecommerce.productservice.dto.ProductResponse;
+import com.ecommerce.productservice.dto.UpdateProductRequest;
+import com.ecommerce.productservice.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/products")
+@RequiredArgsConstructor
+public class ProductController {
+
+    public final ProductService productService;
+
+    @PostMapping
+    public ResponseEntity<ProductResponse> createProduct(
+            @RequestBody CreateProductRequest request) {
+
+        ProductResponse response = productService.createProduct(request);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(response);
+
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductResponse>> getAllProducts() {
+
+        return ResponseEntity.ok(productService.getAllProducts());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResponse> getProduct(@PathVariable UUID id) {
+
+        ProductResponse response = productService.getProduct(id);
+
+        return ResponseEntity.ok(productService.getProduct(id));
+
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductResponse> updateProduct(
+            @PathVariable UUID id,
+            @RequestBody UpdateProductRequest request) {
+
+        return ResponseEntity.ok(productService.updateProduct(id,request));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable UUID id) {
+        productService.deleteProduct(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
+
+
+
+}
